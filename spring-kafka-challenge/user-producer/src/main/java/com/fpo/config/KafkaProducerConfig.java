d383c97d2cecbfc4fc4144bce4028d6bce6a79ef
François Poirier <fpoirier2002@gmail.com>
2021-12-16 16:22:07 +0100

    delete @EnableKafka

--- spring-kafka-challenge/user-producer/src/main/java/com/fpo/config/KafkaProducerConfig.java
+++ spring-kafka-challenge/user-producer/src/main/java/com/fpo/config/KafkaProducerConfig.java
@@ -1,10 +1,8 @@
 package com.fpo.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 
 @Configuration
-@EnableKafka
 public class KafkaProducerConfig {
 
 }
