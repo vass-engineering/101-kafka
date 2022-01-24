bbfac127acf833bda556e485044c3a54fa7503fd
François Poirier <fpoirier2002@gmail.com>
2022-01-24 18:41:53 +0100

    add solution

--- spring-kafka-challenge/all-user-consumer/src/main/java/com/fpo/config/KafkaConsumerConfig.java
+++ spring-kafka-challenge/all-user-consumer/src/main/java/com/fpo/config/KafkaConsumerConfig.java
@@ -1,12 +1,73 @@
 package com.fpo.config;
 
+import com.fpo.model.Customer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableKafka
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String challengeGroupId;
+
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String autoOffsetReset;
+
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+                org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers);
+        props.put(
+                org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        props.put(
+                org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                JsonDeserializer.class);
+        props.put(
+                org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, challengeGroupId);
+        props.put(
+                org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+                autoOffsetReset);
+        props.put(
+                org.apache.kafka.clients.consumer.ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, Customer> consumerFactory() {
+
+        return new DefaultKafkaConsumerFactory<>(
+                consumerConfigs(),
+                new StringDeserializer(),
+                new JsonDeserializer<>(Customer.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Customer>
+    kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Customer> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
 
 
 }
